57d786e4227632eee5ed2a36462cb619cafa3d8f
myvideoyun <developers@myvideoyun.com>
2022-08-15 20:46:26 +0800

    Add green-screen chroma key

--- app/src/main/res/raw/green_screen_fragment.glsl
+++ app/src/main/res/raw/green_screen_fragment.glsl
@@ -5,5 +5,27 @@ uniform sampler2D uSampler;
 varying vec2 vTextureCoord;
 
 void main() {
-  gl_FragColor = texture2D(uSampler, vTextureCoord);
+   // lookup the color of the texel corresponding to the fragment being
+   // generated while rendering a triangle
+   lowp vec4 tempColor = texture2D(uSampler, vTextureCoord);
+
+   // Calculate the average intensity of the texel's red and blue components
+   lowp float rbAverage = tempColor.r * 0.5 + tempColor.b * 0.5;
+
+   // Calculate the difference between the green element intensity and the
+   // average of red and blue intensities
+   lowp float gDelta = tempColor.g - rbAverage;
+
+   // If the green intensity is greater than the average of red and blue
+   // intensities, calculate a transparency value in the range 0.0 to 1.0
+   // based on how much more intense the green element is
+   tempColor.a = 1.0 - smoothstep(0.0, 0.25, gDelta);
+
+   // Use the cube of the of the transparency value. That way, a fragment that
+   // is partially translucent becomes even more translucent. This sharpens
+   // the final result by avoiding almost but not quite opaque fragments that
+   // tend to form halos at color boundaries.
+   tempColor.a = tempColor.a * tempColor.a * tempColor.a;
+
+   gl_FragColor = tempColor;
 }
